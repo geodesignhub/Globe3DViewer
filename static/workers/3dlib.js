e6c63b9e6b3015ff2b57b905c08e33641ce82cec
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2017-05-01 12:10:48 +0100

    Updatd to remove policy

--- static/workers/3dlib.js
+++ static/workers/3dlib.js
@@ -937,13 +937,12 @@ function generateFinal3DGeoms(constraintedModelDesigns, genstreets, existingroad
                             finalGJFeats.push(comfinalFeatures[k1]);
                         }
                     }
+                } else if (curFeat.properties.areatype === 'policy') { // whitelisted policy
+                    var policyF = generatePolicyFeatures(curFeat);
+                    for (var pf = 0; pf < policyF.length; pf++) {
+                        finalGJFeats.push(policyF[pf]);
+                    }
                 }
-                // else if (curFeat.properties.areatype === 'policy') { // whitelisted policy
-                //     var policyF = generatePolicyFeatures(curFeat);
-                //     for (var pf = 0; pf < policyF.length; pf++) {
-                //         finalGJFeats.push(policyF[pf]);
-                //     }
-                // }
 
             }
             // for non white listed systems that are buildings
@@ -1001,13 +1000,12 @@ function generateFinal3DGeoms(constraintedModelDesigns, genstreets, existingroad
                     }
                     curFeat.properties = prop;
                     finalGJFeats.push.apply(finalGJFeats, [curFeat]);
+                } else if (curFeat.properties.areatype === 'policy') {
+                    var policyF = generatePolicyFeatures(curFeat);
+                    for (var pf = 0; pf < policyF.length; pf++) {
+                        finalGJFeats.push(policyF[pf]);
+                    }
                 }
-                // else if (curFeat.properties.areatype === 'policy') {
-                //     var policyF = generatePolicyFeatures(curFeat);
-                //     for (var pf = 0; pf < policyF.length; pf++) {
-                //         finalGJFeats.push(policyF[pf]);
-                //     }
-                // }
             }
 
         }
