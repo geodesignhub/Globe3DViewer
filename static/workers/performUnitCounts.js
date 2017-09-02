c338e8c07d5ca15b50ff8f8d03e334807214ea21
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2017-09-02 21:34:54 +0100

    Updated Cesium build

--- static/workers/performUnitCounts.js
+++ static/workers/performUnitCounts.js
@@ -1,27 +1,27 @@
-function unitCountonFeatures(allFeaturesList) {
+function unitCountonFeatures(allFeaturesList, systems) {
     // constrain output ot only features in the list. 
     var constraintedFeatures = { "type": "FeatureCollection", "features": [] };
     var allFeatures = JSON.parse(allFeaturesList);
+    var systems = JSON.parse(systems);
+    var sysUnits = {};
+    for (var i = 0; i < systems.length; i++) {
+        var element = systems[i];
+        sysUnits[element['sysname']] = 0;
+    }
 
     var af = allFeatures.features;
     var featlen = af.length;
     var counter = 0;
-    var sysUnits = {};
     var fullproc = featlen;
     for (var d = 0; d < featlen; d++) {
         var curfeatprop = af[d].properties;
         var curFeatSys = curfeatprop.sysname;
         var isStreet = curfeatprop.isStreet;
         if (isStreet) {} else {
-            if (sysUnits.hasOwnProperty(curFeatSys)) {
-                sysUnits[curFeatSys] += curfeatprop.totalunits;
-            } else {
 
-                sysUnits[curFeatSys] = 0;
-                if (curfeatprop.hasOwnProperty(totalunits)) {
-                    if (curfeatprop.totalunits === parseInt(curfeatprop.totalunits, 10)) {
-                        sysUnits[curFeatSys] = curfeatprop.totalunits;
-                    }
+            if (curfeatprop.hasOwnProperty('totalunits')) {
+                if (curfeatprop.totalunits === parseInt(curfeatprop.totalunits, 10)) {
+                    sysUnits[curFeatSys] += curfeatprop.totalunits;
                 }
             }
         }
@@ -38,5 +38,5 @@ function unitCountonFeatures(allFeaturesList) {
 }
 
 self.onmessage = function(e) {
-    unitCountonFeatures(e.data.allFeaturesList);
+    unitCountonFeatures(e.data.allFeaturesList, e.data.systems);
 }
