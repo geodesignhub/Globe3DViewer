98e61e3d485bbb66874c5f2d2cef75e769747b09
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2021-04-12 20:21:03 +0100

    Update processor.js

--- processor.js
+++ processor.js
@@ -1,5 +1,8 @@
 // processor.js
-var redisclient = require('redis').createClient(process.env.REDIS_URL || { host: '127.0.0.1', port: 6379 });
+var redisclient = require('redis').createClient(process.env.REDIS_URL || {
+    host: '127.0.0.1',
+    port: 6379
+});
 // redisclient.on('ready', function() {
 //     console.log("Redis is ready");
 // });
@@ -8,39 +11,48 @@ var redisclient = require('redis').createClient(process.env.REDIS_URL || { host:
 //     console.log("Error in Redis");
 // });
 var tools = require('./3Dprocessor/tools');
-module.exports = function(job) {
+module.exports = function (job) {
     // Do some heavy work
-    const synthesisid =job.data.synthesisid;
+    const synthesisid = job.data.synthesisid;
     // var existingroads = job.data.rfc;
-    
+
     var constraintedDesigns = job.data.gj;
-    
+
     const systems = job.data.sys;
 
-    
+
     // if (existingroads.features.length > 0) {
     //     existingroads = tools.bufferExistingRoads(existingroads);
     // }
-   
-    
+
+
     var curFeats = constraintedDesigns.features;
     var flen = curFeats.length;
     var fullproc = flen;
     var counter = 0;
     var finalGJFeats = [];
-    
-    
+
+
     for (var h = 0; h < flen; h++) {
-        
+        var cur3DGeom = [];
         // for every feature , create a point grid.
         var curFeat = curFeats[h];
-        const cur3DGeom = tools.generateFinal3DGeoms(curFeat, 0);
-        finalGJFeats.push.apply(finalGJFeats, cur3DGeom);
+        try {
+            cur3DGeom = tools.generateFinal3DGeoms(curFeat, 0);
+        } catch (error) {
+            console.log(error);
+        }
+        if (cur3DGeom) {
+            finalGJFeats.push.apply(finalGJFeats, cur3DGeom);
+        }
         counter += 1;
-        job.progress({'percent': parseInt((100 * counter) / fullproc),'synthesisid':synthesisid} );
-            
+        job.progress({
+            'percent': parseInt((100 * counter) / fullproc),
+            'synthesisid': synthesisid
+        });
+
     }
-    
+
     var final3DGeoms = {
         "type": "FeatureCollection",
         "features": finalGJFeats
@@ -51,11 +63,14 @@ module.exports = function(job) {
     // job.progress(50);
 
     var center = tools.generateCenter(constraintedDesigns);
-    
+
     // console.log(center, unitCounts);
     // job.progress(100);
     console.log('Computation Complete..');
-    redisclient.set(synthesisid, JSON.stringify({ "finalGeoms": final3DGeoms, "center": center }));
+    redisclient.set(synthesisid, JSON.stringify({
+        "finalGeoms": final3DGeoms,
+        "center": center
+    }));
 
     return Promise.resolve(synthesisid);
 }
