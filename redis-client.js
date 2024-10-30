e802830fa849f8f222c4474e3def5a399a547288
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2024-10-30 15:10:59 +0000

    Update redis-client.js

--- redis-client.js
+++ redis-client.js
@@ -4,41 +4,43 @@ require("dotenv").config();
 const REDIS_URL = process.env.REDIS_URL || "redis://127.0.0.1:6379/";
 
 
-  const redisOptions = REDIS_URL.includes("rediss://")
-    ? {
-        url: REDIS_URL,  
-        socket:{ tls: {
-          rejectUnauthorized: false
-        }},
+const redisOptions = REDIS_URL.includes("rediss://")
+  ? {
+    url: REDIS_URL,
+    socket: {
+      tls: {
+        rejectUnauthorized: false
       }
-    : REDIS_URL;
+    },
+  }
+  : REDIS_URL;
 
-    
+
 const redis_client = redis.createClient(redisOptions);
 function redis_error_handler(err) {
-    console.debug(`node-redis version is ${require('redis/package.json').version}`);
-    console.debug(err);
+  console.debug(`node-redis version is ${require('redis/package.json').version}`);
+  console.debug(err);
 }
 
 (async () => {
-    redis_client.on('error', (err) => {
-        console.debug('Error from Redis Client:', err);
-        process.exit(1);
-    });
-    redis_client.on('ready', () => console.debug('Redis is ready..'));
+  redis_client.on('error', (err) => {
+    console.debug('Error from Redis Client:', err);
+    process.exit(1);
+  });
+  redis_client.on('ready', () => console.debug('Redis is ready..'));
 
-    const connect = await redis_client.connect().catch(redis_error_handler);;
-    const pong = await redis_client.ping().catch(redis_error_handler);
+  const connect = await redis_client.connect().catch(redis_error_handler);;
+  const pong = await redis_client.ping().catch(redis_error_handler);
 })();
 
 module.exports = {
-    ...redis_client,
-    get: (redis_client.get).bind(redis_client),
-    hget: (redis_client.hGet).bind(redis_client),
-    hgetall: (redis_client.hGetAll).bind(redis_client),
-    expire: (redis_client.expire).bind(redis_client),
-    set: (redis_client.set).bind(redis_client),
-    mset: (redis_client.mSet).bind(redis_client),
-    hset: (redis_client.hSet).bind(redis_client),
+  ...redis_client,
+  get: (redis_client.get).bind(redis_client),
+  hget: (redis_client.hGet).bind(redis_client),
+  hgetall: (redis_client.hGetAll).bind(redis_client),
+  expire: (redis_client.expire).bind(redis_client),
+  set: (redis_client.set).bind(redis_client),
+  mset: (redis_client.mSet).bind(redis_client),
+  hset: (redis_client.hSet).bind(redis_client),
 
 };
