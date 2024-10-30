22eb03d0ff254c118327b58cde047da0035e187c
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2024-10-30 18:21:02 +0000

    Update server.js

--- server.js
+++ server.js
@@ -213,7 +213,7 @@
         }
 
     });
-    const channels = [];
+    let channels = [];
     let tmp_session_id = "";
     function uuidv4() {
         return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
