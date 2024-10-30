34fe64af13208b699527324750c591b435469c62
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2024-10-30 15:22:52 +0000

    Update redis-client.js

--- redis-client.js
+++ redis-client.js
@@ -1,6 +1,6 @@
 const redis = require('redis');
 require("dotenv").config();
-const REDIS_URL = process.env.REDIS_TEMPORARY_URL || "redis://127.0.0.1:6379/";
+const REDIS_URL = process.env.REDIS_URL || "redis://127.0.0.1:6379/";
 
 // const redisOptions = REDIS_URL.includes("rediss://")
 //   ? {
